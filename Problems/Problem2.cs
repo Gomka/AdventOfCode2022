af48bdb757b03cb0d39ae15c5cdcc11690103945
Gonzalo Marcos Vaquero <gmarcosvaquero@gmail.com>
2022-12-01 20:20:00 +0100

    Cleanup + problem 2 ready to be solved

---
+++ Problems/Problem2.cs
@@ -0,0 +1,12 @@
+namespace advent2022 {
+    public class Problem2 {
+
+        public string Question1() {
+            return "";
+        }
+
+        public string Question2() {
+            return "";
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -4,8 +4,15 @@ using advent2022;
 Console.WriteLine("\n\n                     Advent of Code 2022                     \n");
 
 #region Problem1
-Console.WriteLine("\n-------- Problem 1:\n");
-Problem1 problem1 = new Problem1();
-Console.WriteLine("Solution 1: " + problem1.Question1());
-Console.WriteLine("Solution 1: " + problem1.Question2());
+    Console.WriteLine("\n-------- Problem 1:\n");
+    Problem1 problem1 = new Problem1();
+    Console.WriteLine("Solution 1: " + problem1.Question1());
+    Console.WriteLine("Solution 2: " + problem1.Question2());
+#endregion
+
+#region Problem2
+    Console.WriteLine("\n-------- Problem 2:\n");
+    Problem2 problem2 = new Problem2();
+    Console.WriteLine("Solution 1: " + problem2.Question1());
+    Console.WriteLine("Solution 2: " + problem2.Question2());
 #endregion
