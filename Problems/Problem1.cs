04d1cbfa3217a4710746471e006f8527c153b970
Gonzalo Marcos Vaquero <gmarcosvaquero@gmail.com>
2022-12-01 19:58:59 +0100

    Problem 1 Question 1

--- Problems/Problem1.cs
+++ Problems/Problem1.cs
@@ -2,7 +2,22 @@ namespace advent2022 {
     public class Problem1 {
 
         public string Question1() {
-            return "";
+            string[] lines = System.IO.File.ReadAllLines(@"Data\input1.1.txt");
+
+            int maxCal = 0;
+            int localCal = 0;
+
+            foreach (string str in lines)
+            {
+                if(str.Length == 0) {
+                    if(localCal > maxCal) maxCal = localCal;
+                    localCal = 0;
+                } else {
+                    localCal += Int32.Parse(str);  
+                }
+            }
+            
+            return maxCal.ToString();
         }
 
         public string Question2() {
