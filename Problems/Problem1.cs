a258768719bce0dcf37a61216fbb9745f42bb833
Gonzalo Marcos Vaquero <gmarcosvaquero@gmail.com>
2022-12-01 19:58:34 +0100

    Code setup completed

---
+++ Problems/Problem1.cs
@@ -0,0 +1,12 @@
+namespace advent2022 {
+    public class Problem1 {
+
+        public string Question1() {
+            return "";
+        }
+
+        public string Question2() {
+            return "";
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using advent2022;
+
+Console.WriteLine("\n\n                     Advent of Code 2022                     \n");
+
+#region Problem1
+Console.WriteLine("\n-------- Problem 1:\n");
+Problem1 problem1 = new Problem1();
+Console.WriteLine("Solution 1: " + problem1.Question1());
+Console.WriteLine("Solution 1: " + problem1.Question2());
+#endregion
