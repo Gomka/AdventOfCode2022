b6db3f9cab13f1a9f163e088980cc0e42ac8b578
Gonzalo Marcos Vaquero <gmarcosvaquero@gmail.com>
2022-12-02 09:39:40 +0100

    Problem 2 Question 2

--- Problems/Problem2.cs
+++ Problems/Problem2.cs
@@ -37,7 +37,50 @@ namespace advent2022 {
         }
 
         public string Question2() {
-            return "";
+            string[] lines = System.IO.File.ReadAllLines(@"Data\input2.1.txt");
+
+            int score = 0;
+            char currentPlay;
+
+            foreach (string str in lines)
+            {
+                currentPlay = str.ElementAt(2);
+
+                switch (currentPlay)
+                {
+                    case 'X': //lose
+                        if(str.ElementAt(0) == 'A') {
+                            score += 3;
+                        } else if(str.ElementAt(0) == 'B') {
+                            score += 1;
+                        } else {
+                            score += 2;
+                        }
+                        break;
+                    case 'Y': //draw
+                        if(str.ElementAt(0) == 'A') {
+                            score += 4;
+                        } else if(str.ElementAt(0) == 'B') {
+                            score += 5;
+                        } else {
+                            score += 6;
+                        }
+                        break;
+                    case 'Z': //win
+                        if(str.ElementAt(0) == 'A') {
+                            score += 8;
+                        } else if(str.ElementAt(0) == 'B') {
+                            score += 9;
+                        } else {
+                            score += 7;
+                        }
+                        break;
+                    default:
+                        break;
+                }
+            }
+
+            return score.ToString();
         }
     }
 }
