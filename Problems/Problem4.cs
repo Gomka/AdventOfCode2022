71a4096712e898670855a73361bfecf3f3918e69
Gonzalo Marcos Vaquero <gmarcosvaquero@gmail.com>
2022-12-04 12:20:28 +0100

    Problem 4 Question 2

--- Problems/Problem4.cs
+++ Problems/Problem4.cs
@@ -21,7 +21,26 @@ namespace advent2022 {
         }
 
         public string Question2() {
-            return "";
+            string[] lines = System.IO.File.ReadAllLines(@"Data\input4.1.txt");
+            int count = 0;
+
+            foreach (string str in lines) {
+                string[] parts = str.Split(",");
+                string[] first = parts[0].Split("-");
+                string[] last = parts[1].Split("-");
+
+                if(Int32.Parse(last[0]) >= Int32.Parse(first[0]) && Int32.Parse(last[0]) <= Int32.Parse(first[1])) {
+                    count++;
+                } else if(Int32.Parse(last[1]) >= Int32.Parse(first[0]) && Int32.Parse(last[1]) <= Int32.Parse(first[1])) {
+                    count++;
+                } else if(Int32.Parse(first[0]) >= Int32.Parse(last[0]) && Int32.Parse(first[0]) <= Int32.Parse(last[1])) {
+                    count++;
+                } else if(Int32.Parse(first[1]) >= Int32.Parse(last[0]) && Int32.Parse(first[1]) <= Int32.Parse(last[1])) {
+                    count++;
+                }
+            }
+            
+            return count.ToString();
         }
     }
 }
